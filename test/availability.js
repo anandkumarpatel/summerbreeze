ec263ef6e0bbda64a74e230118c9f32282c3e26e
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-01-22 00:15:54 -0800

    add availability test

--- test/availability.js
+++ test/availability.js
@@ -1,28 +1,124 @@
-//test cases
-//012345
-//  ##   2    3
-// ##    ci=2 co>1
-//   ##  ci<4 co=3
-//  ## < ci<3 co>2
-//    ## ci<5 co=4
-//##     ci<1 co=0
-// ### < ci<3 co>1
-//  ###< ci<4 co>2
-// ####< ci<4 co>1
+'use strict';
+require('../lib/loadConfig.js');
+var expect = require('code').expect;
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var beforeEach = lab.beforeEach;
+var afterEach = lab.afterEach;
+var it = lab.test;
 
+var request = require('request');
 
+var app = require('../lib/app.js');
+var mongo = require('../lib/helpers/mongo.js');
+var R = require('./reservations-fixtures.js');
+var G = require('./guests-fixtures.js');
+var Room = require('./rooms-fixtures.js');
 
-//0123456
-//  ###   2    4
-//     ## ci<6 co=5
-//##      ci<1 co=0
-// ##     ci=2 co>1
-//    ##  ci<5 co=4
-// ###  < ci<3 co>1
-// #### < ci<4 co>1
-// #####< ci<5 co>1
-//   ###< ci<5 co>3
-//  ####< ci<5 co>2
-//  ### < ci<4 co>2
-//  ##  < ci<3 co>2
-//   ## < ci<4 co>3
+function getAvailablility(query, cb) {
+  request({
+    method: 'GET',
+    url: 'http://localhost:' + process.env.PORT + '/availability',
+    qs: query
+  }, cb);
+}
+
+function createReservationForRange(ctx, name, checkIn, checkOut, cb) {
+  var info = {
+    checkIn: new Date('1/' + checkIn + '/2005').getTime(),
+    checkOut: new Date('1/' + checkOut + '/2005').getTime(),
+    rate: 100,
+    paymentType: R.C.paymentType.cash,
+    status: R.C.status.notIn,
+    comment: 'late checking'
+  };
+  G.createRandomGuest(function(err, guest) {
+    if (err) {
+      return cb(err);
+    }
+    info.guests = [guest._id];
+    Room.testRoom.number = checkIn + checkOut * 10;
+    Room.postRoom(Room.testRoom, function(err, res, room) {
+      if (err) {
+        return cb(err);
+      }
+      info.rooms = [room._id];
+      R.postReservation(info, function(err, res, body) {
+        if (err) {
+          return cb(err);
+        }
+        ctx[name] = body;
+        cb(null, body);
+      });
+    });
+  });
+}
+
+describe('Availablility', function() {
+  var ctx = {};
+  beforeEach(function(done) {
+    ctx = {};
+    done();
+  });
+  beforeEach(app.start);
+  beforeEach(function(done) {
+    mongo.dropDatabase(done);
+  });
+  //test cases 15 rooms total
+  // 1/1/2005 - 1/6/2005
+  ///////////////
+  //123456|
+  //##----| 12
+  //###---| 13
+  //####--| 14 X
+  //#####-| 15 X
+  //######| 16 X
+  //-##---| 23
+  //-###--| 24 X
+  //-####-| 25 X
+  //-#####| 26 X
+  //--##--| 34 X
+  //--###-| 35 X
+  //--####| 36 X
+  //---##-| 45 X
+  //---###| 46 X
+  //----##| 56
+  //--TTT-| test
+  describe('long list of reservations', function() {
+    function makeReservation (i,j) {
+      return function(done) {
+        createReservationForRange(ctx, 'r' + (i * 10 + j), i, j, done);
+      };
+    }
+    for (var i = 1; i < 6; i++) {
+      for (var j = i + 1; j < 7; j++) {
+        beforeEach(makeReservation(i,j));
+      }
+    }
+    afterEach(app.stop);
+    it('should list all reservations', function(done) {
+      R.getReservation(null, function(err, res, body) {
+        body = JSON.parse(body);
+        expect(body.length).to.equal(15);
+        done();
+      });
+    });
+    it('should list only overlapping reservations 1/3-5/2015', function(done) {
+      getAvailablility({
+        checkIn: new Date('1/3/2005').getTime(),
+        checkOut: new Date('1/5/2005').getTime()
+      }, function(err, res, body) {
+        body = JSON.parse(body);
+        expect(body.length).to.equal(4);
+        expect(body).to.contain(ctx.r12.rooms[0]);
+        expect(body).to.contain(ctx.r13.rooms[0]);
+        expect(body).to.contain(ctx.r23.rooms[0]);
+        expect(body).to.contain(ctx.r56.rooms[0]);
+
+        done();
+      });
+    });
+  });
+  // check reservations with multi rooms
+});
